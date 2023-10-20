b16136ad48f144a9c8d368cd2ac1f7d5ae1d5e39
daniil-torubara <dabarara@bk.ru>
2023-10-20 18:33:55 +0600

    stage 1 module 5 task 3

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -1,27 +1,43 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CollectionService {
 
     public List<Integer> filterEvenNumbers(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<Integer> listInt = list.stream().filter(num -> num % 2 == 0).collect(Collectors.toList());
+        return listInt;
     }
 
     public List<String> toUpperCaseCollection(List<String> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> newList = list.stream().map(word -> word.toUpperCase()).collect(Collectors.toList());
+        return newList;
     }
 
     public Optional<Integer> findMax(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        int maxNumber = list.stream().mapToInt(num -> num).max().orElseThrow(NoSuchElementException::new);
+        return Optional.of(maxNumber);
     }
 
     public Optional<Integer> findMin(List<List<Integer>> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        int minNumber = list.stream().mapToInt(
+                innerList -> {
+                    return innerList
+                            .stream()
+                            .mapToInt(num -> num)
+                            .min()
+                            .orElseThrow(NoSuchElementException::new);
+                }
+                ).min().orElseThrow(NoSuchElementException::new);
+        return Optional.of(minNumber);
     }
 
     public Integer sum(List<Integer> list) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        int sumResult = list.stream().reduce(0, (subtotal, element) -> subtotal + element);
+        return sumResult;
     }
 }
